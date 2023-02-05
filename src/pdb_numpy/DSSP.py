6599d5041b6cd4f40ec13de14b52ea87c2df5724
samuelmurail <samuel.murail@gmail.com>
2023-02-05 01:50:15 +0100

    Almost perfect DSSP

--- src/pdb_numpy/DSSP.py
+++ src/pdb_numpy/DSSP.py
@@ -51,7 +51,8 @@ def add_NH(coor):
             CA_N = CA_N / np.linalg.norm(CA_N)
 
             NH = C_N + CA_N
-            NH = (NH / np.linalg.norm(NH)) * 0.9970  # Charmm36
+            # NH = (NH / np.linalg.norm(NH)) * 0.9970  # Charmm36
+            NH = (NH / np.linalg.norm(NH)) * 1.01  # ?
             NH += N
 
             model.add_atom(N_index + 1, 
@@ -111,8 +112,57 @@ def hbond_energy(vec_N, vec_H, vec_O, vec_C):
 
     return energy
 
+def compute_bend(CA_sel):
+    """Compute bend for a protein.
+
+    Parameters
+    ----------
+    CA_sel : AtomGroup
+        AtomGroup containing only CA atoms.
+
+    Returns
+    -------
+    bend : numpy.ndarray
+        Array of bend values.
+    """
+
+    n_res = len(CA_sel.uniq_resid)
+    bend = np.array([False for _ in range(n_res)])
+
+    for i in range(2, n_res - 2):
+
+        CA_i = CA_sel.xyz[i]
+        CA_i_minus_2 = CA_sel.xyz[i - 2]
+        CA_i_plus_2 = CA_sel.xyz[i + 2]
+
+        vec_i_1 = CA_i - CA_i_minus_2
+        vec_i_2 = CA_i_plus_2 - CA_i
+
+        vec_i_1 = vec_i_1 / np.linalg.norm(vec_i_1)
+        vec_i_2 = vec_i_2 / np.linalg.norm(vec_i_2)
+
+        #print(i, np.degrees(geom.angle_vec(vec_i_1, vec_i_2)))
+        if np.degrees(geom.angle_vec(vec_i_1, vec_i_2)) > 70.:
+            bend[i] = True
+
+    return bend
+
 def compute_DSSP(coor):
     """Compute DSSP for a protein.
+
+
+    Code:
+    ----------
+    “H” = 4-helix ($\alpha$-helix)
+    “B” = residue in isolated $\beta$-bridge
+    “E” - extended strand. participates in $\beta$-ladder
+    “G” = 3-helix ($3_{10}$-heIix)
+    “I” = 5-helix ($\pi$-helix)
+    “T” = H-bonded turn
+    “S” = bend
+
+    In case of structural overlaps. priority is given to the structure first in
+    this list.
     """
 
     cutoff = 8
@@ -128,7 +178,7 @@ def compute_DSSP(coor):
     for model in coor.models:
         # Compute distance matrix between all residues
         dist_mat = distance_matrix(CA_sel.xyz, CA_sel.xyz)
-        Hbond_mat = np.zeros_like(dist_mat)
+        Hbond_mat = np.zeros_like(dist_mat, dtype=bool)
 
         # Compute HBond matrix
         for i in range(n_res):
@@ -154,45 +204,99 @@ def compute_DSSP(coor):
                 if len(O_i) == 1 and len(N_j) == 1 and len(C_i) == 1 and len(H_j) == 1:
                     energy = hbond_energy(N_j, H_j, O_i, C_i)
                     if energy < -0.5:
-                        Hbond_mat[i, j] = 1
+                        Hbond_mat[i, j] = True
                 
                 if len(O_j) == 1 and len(N_i) == 1 and len(C_j) == 1 and len(H_i) == 1:
                     energy = hbond_energy(N_i, H_i, O_j, C_j)
                     if energy < -0.5:
-                        Hbond_mat[j, i] = 1
+                        Hbond_mat[j, i] = True
 
         # Compute secondary structure
-        SS_seq = []
-
+        SS_seq = np.array([" " for i in range(n_res)])
+        H_seq = np.array([False for i in range(n_res)])
+        G_seq = np.array([False for i in range(n_res)])
+        I_seq = np.array([False for i in range(n_res)])
+        E_seq = np.array([False for i in range(n_res)])
+        S_seq = compute_bend(CA_sel)
+    
         # N-turn
-        for i in range(n_res):
-            if i < n_res - 4 and Hbond_mat[i, i + 4] == 1:
-                SS_seq.append("H")
-            elif i < n_res - 3 and Hbond_mat[i, i + 3] == 1:
-                SS_seq.append("G")
-            elif i < n_res - 5 and Hbond_mat[i, i + 5] == 1:
-                SS_seq.append("I")
-            else:
-                SS_seq.append(" ")
+        for i in range(n_res - 3):
+            if i < n_res - 4 and Hbond_mat[i, i + 4]:
+                H_seq[i] = True
+            if Hbond_mat[i, i + 3]:
+                G_seq[i] = True
+            if i < n_res - 5 and Hbond_mat[i, i + 5]:
+                I_seq[i] = True
         
         # Beta sheet
         for i in range(1, n_res-1):
             for j in range(i+3, n_res-1):
-                if (Hbond_mat[i-1, j] and Hbond_mat[j, i+1]) or (Hbond_mat[j-1, i] and Hbond_mat[i, j+1]):
-                    SS_seq[i] = "E"
-                    SS_seq[j] = "E"
-                if (Hbond_mat[i, j] and Hbond_mat[j, i]) or (Hbond_mat[i-1, j+1] and Hbond_mat[j-1, i+1]):
-                    SS_seq[i] = "e"
-                    SS_seq[j] = "e"
+                if ((Hbond_mat[i-1, j] and Hbond_mat[j, i+1]) or (Hbond_mat[j-1, i] and Hbond_mat[i, j+1])) or\
+                   ((Hbond_mat[i, j] and Hbond_mat[j, i]) or (Hbond_mat[i-1, j+1] and Hbond_mat[j-1, i+1])):
+                    E_seq[i] = True
+                    E_seq[j] = True
                 
+        # Assign secondary structure sequence (order follows the list above)
+        # Bend
+        for i in range(n_res):
+            if S_seq[i]:
+                SS_seq[i] = "S"
+        
+        for i in range(n_res - 1):
+            if I_seq[i]:
+                SS_seq[i +1 : i + 5] = "T"
+            if H_seq[i]:
+                SS_seq[i + 1 : i + 4] = "T"
+            if G_seq[i]:
+                SS_seq[i + 1 : i + 3] = "T"
+
+        # A minimal helix is defined by two consecutive n-turns
+
+        for i in range(1, n_res-3):
+            if G_seq[i] and G_seq[i - 1]:
+                SS_seq[i: i + 3] = "G"
+
+        for i in range(1, n_res-1):
+            if E_seq[i] and E_seq[i - 1]:
+                SS_seq[i - 1: i + 1] = "E"
+            elif E_seq[i] and not (E_seq[i + 1] and E_seq[i - 1]):
+                SS_seq[i] = "B"
+        
+
+
+        for i in range(1, n_res-4):
+            # A minimal helix is defined by two consecutive n-turns
+            if H_seq[i] and H_seq[i - 1]:
+                SS_seq[i: i + 4] = "H"
+        #  In 2012, DSSP was rewritten so that the assignment of π helices
+        #  was given preference over α helices, resulting in better detection of π helices.
+        for i in range(1, n_res-5):
+            if I_seq[i] and I_seq[i - 1]:
+                SS_seq[i: i + 5] = "I"
+
+        # Two overlapping minimal helices offset by two or three residues are joined
+        # into one helix:
+        for i in range(1, n_res-2):
+            if SS_seq[i -1] == "H" and (SS_seq[i + 1] == "H" or SS_seq[i + 2] == "H"):
+                SS_seq[i] = "H"
+
+        for i in range(1, n_res-1):
+            if SS_seq[i -1] in ["E", "B"] and (SS_seq[i +1] in ["E", "B"]):
+                SS_seq[i-1:i+2] = "E"
+        #for i in range(n_res - 2, 1, -1):
+        #    if SS_seq[i] == "G" and SS_seq[i-1] == "G":
+        #        SS_seq[i + 1: i + 3] = "G"
+        #        if SS_seq[i - 2] != "G":
+        #            SS_seq[i - 1] = " "
 
         # Fix Helix with 1/2 gaps
-        for i in range(1, n_res - 2):
-            if SS_seq[i] == " " and SS_seq[i - 1] == "H" and (SS_seq[i + 1] == "H" or SS_seq[i + 2] == "H"):
-                SS_seq[i] = "H"
+        #for i in range(1, n_res - 2):
+        #    if SS_seq[i] == " " and SS_seq[i - 1] == "H" and (SS_seq[i + 1] == "H" or SS_seq[i + 2] == "H"):
+        #        SS_seq[i] = "H"
         SS_seq = "".join(SS_seq)
 
         print(SS_seq)
+        print(len(SS_seq))
     print(max_dist)
 
 
--- test.py
+++ test.py
@@ -4,17 +4,41 @@ sys.path.insert(0, './src')
 import pdb_numpy.coor
 import pdb_numpy.DSSP as DSSP
 
-test = pdb_numpy.Coor("./src/pdb_numpy/tests/input/1rxz.pdb")
+#test = pdb_numpy.Coor("./src/pdb_numpy/tests/input/1rxz.pdb")
+test = pdb_numpy.Coor(pdb_id="3eam")
+test.write_pdb("./tmp.pdb", check_file_out=False)
 
 print(test.len)
 
+print('Adding NH')
 DSSP.add_NH(test)
+print('NH added')
 
-test.write_pdb("./tmp.pdb", check_file_out=False)
+# run dssp:
+# sudo apt install dssp
+# dssp -i tmp.pdb -o dssp.txt
 
+with open("./dssp.txt") as f:
+    lines = f.readlines()
+
+start_read = False
+dssp_seq = ""
+for line in lines:
+    if line.startswith("  #  RESIDUE AA STRUCTURE BP1 BP2  ACC"):
+        start_read = True
+        continue
+    if start_read and line[13] != "!":
+        dssp_seq += line[16]
+
+print(dssp_seq)
+print(len(dssp_seq))
+
+
+print('Computing DSSP')
 matrix = DSSP.compute_DSSP(test)
+print('DSSP computed')
 
 import matplotlib.pyplot as plt
 
-plt.imshow(matrix)
-plt.show()
+#plt.imshow(matrix)
+#plt.show()

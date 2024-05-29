2f2c753dbae63c0e8c16de21c6d332cf60769239
samuelmurail <samuel.murail@univ-paris-diderot.fr>
2024-05-29 11:31:02 +0200

    Bump version: 0.0.4 → 0.0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import numpy as np
 from setuptools import setup
 from distutils.extension import Extension
 
-version="0.0.4"
+version="0.0.5"
 
 with open('README.rst', encoding='utf-8') as readme_file:
     readme = readme_file.read()

34d67d18c929dc1f9941a705bf9a039fa9792d0c
samuelmurail <samuel.murail@univ-paris-diderot.fr>
2024-05-29 10:10:39 +0200

    Patch 0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import numpy as np
 from setuptools import setup
 from distutils.extension import Extension
 
-version="0.0.2"
+version="0.0.3"
 
 with open('README.rst', encoding='utf-8') as readme_file:
     readme = readme_file.read()
